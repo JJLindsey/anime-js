7e9940c6083cce1fd4c4d66006f3f4916086b640
JJLindsey <jlindsey010@gmail.com>
2022-01-20 17:48:50 -0800

    various examples for animating in js

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-const squares = document.getElementsByClassName('square')
-const allElments = document.querySelectorAll('div')
+//const squares = document.getElementsByClassName('square')
+// const allElments = document.querySelectorAll('div')
+//const squareStats = document.querySelector('#stats')
 
+//animate distance
+// const squareDistance = {
+//     distance: 0,
+//     progress: '0%'
+// }
+
+// anime({
+//     targets: squareDistance,
+//     distance: 300,
+//     progress: '100%',
+//     update: function() {
+//         //JSON displays object property values
+//         squareStats.innerHTML = JSON.stringify(squareDistance)
+//     },
+//     //rounds to whole number
+//     round: 1,
+//     easing: 'linear',
+// })
+
+// anime({
+//     targets: '.square',
+//     left: 300,
+// })
+//------ END ANIMATE DIST-----
 
 // anime({
 //     targets: allElments[2],
@@ -17,15 +42,24 @@ const allElments = document.querySelectorAll('div')
 // })
 
 //target all elements
-anime({
-    targets: [squares, '.circle'],
-    left: 300,
-    rotate: 45,
-    // scale: 0.5,
-    opacity: 0.4,
-})
+// anime({
+//     targets: [squares, '.circle'],
+//     left: 300,
+//     rotate: 45,
+//     // scale: 0.5,
+//     opacity: 0.4,
+// })
 
+// anime({
+//     targets: squares,
+//     borderRadius: 50,
+// })
+
+//------ START ANIMATE NUMBERS----
 anime({
-    targets: squares,
-    borderRadius: 50,
+    targets: '.countUp',
+    value: [5,0],
+    round: 1,
+    easing: 'linear',
+    duration: 3000,
 })
